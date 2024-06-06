2249dd8e8de6bd0e37f9c2260064a5711683273c
Sam Ford <samueljamesford@googlemail.com>
2024-06-06 14:46:50 +0100

    Adds Bcrypt to Login

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -1,24 +1,33 @@
 const User = require("../models/user");
-const TokenGenerator = require("../lib/token_generator")
+const TokenGenerator = require("../lib/token_generator");
+const bcrypt = require("bcrypt");
 
 const AuthenticationController = {
-
-  Authenticate: (req, res) => {
+  Authenticate: async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    User.findOne({ email: email }).then((user) => {
+    try {
+      const user = await User.findOne({ email: email }).collation({ locale: 'en', strength: 2 });
+
       if (!user) {
-        console.log("auth error: user not found")
-        res.status(401).json({ message: "auth error" });
-      } else if (user.password !== password) {
-        console.log("auth error: passwords do not match")
-        res.status(401).json({ message: "auth error" });
-      } else {
-        const token = TokenGenerator.jsonwebtoken(user.id)
-        res.status(201).json({ token: token, message: "OK" });
+        return res.status(401).json({ message: "Email address not recognised" });
       }
-    });
+
+      const PasswordMatch = await bcrypt.compare(password, user.password);
+    
+      if (!PasswordMatch) {
+        return res.status(401).json({ message: "Incorrect password" });
+      }
+
+      const token = TokenGenerator.jsonwebtoken(user.id);
+      return res.status(201).json({ token: token, message: "OK" });
+
+    } 
+    catch (error) {
+      console.error('Error during authentication:', error);
+      return res.status(500).json({ message: "Internal server error", error: error.message });
+    }
   }
 };
 
